19ddb9e1145cfe7de6cfd911424ff7487ab18c97
“xunzhang” <1452356370@qq.com>
2021-06-06 17:16:57 +0800

    first commit

---
+++ src/test/java/com/lemon/HelloWord.java
@@ -0,0 +1,16 @@
+package com.lemon;
+
+/**
+ * @Project: idea_test
+ * @Site: http://www.lemonban.com
+ * @Forum: http://testingpai.com
+ * @Copyright: ©2021 版权所有 勋章
+ * @Author: xunzhang
+ * @Create: 2021-06-06 17:08
+ * @Desc
+ **/
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("hello lemon");
+    }
+}
